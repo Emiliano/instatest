6f7232a5f834e5784b898e1d398b2b4adc8b3690
Erick Emiliano <erick@outlook.sa>
2017-02-13 15:38:23 -0200

    changed to insta-api

--- app.js
+++ app.js
@@ -1,27 +1,25 @@
 /**
  * Created by Emiliano on 12/02/17.
- * Docs: https://github.com/totemstech/instagram-node
- * https://www.npmjs.com/package/instagram-node
+ * Docs: https://github.com/guilhermefarias/instagram-api
+ * https://www.npmjs.com/package/instagram-api
  */
 
 const express = require('express'),
-      ig = require('instagram-node').instagram(),
+      InstagramAPI = require('instagram-api'),
+      access_token = '240993422.e3987d8.3b585871d3714523a6af06421d001eb8',
+      client_id = 'e3987d83c96f4646885afc6a5abc9c5e',
+      client_secret = '4a1f6a89eeda434d8b7fb0aa469a3d59',
+      ig = new InstagramAPI(access_token),
       app = express();
 
-
-ig.use({ access_token: '240993422.e3987d8.3b585871d3714523a6af06421d001eb8' });
-ig.use({
-    client_id: 'e3987d83c96f4646885afc6a5abc9c5e',
-    client_secret: '4a1f6a89eeda434d8b7fb0aa469a3d59'
-});
-
-
 app.get('/', function(req, res) {
-    ig.tag_search('query', function(err, result, remaining, limit) {
-        if (err)
-            console.log(err);
-        else
-            console.log(result);
+    ig.user_search('cpbr10', function (err, result, remaining, limit) {
+        console.log(result)
+
+        ig.user_media_recent(result, function (err, result2, remaining, limit) {
+            res.send(result2)
+        });
+
     });
 });
 
